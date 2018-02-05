d214a800e9c2a383a4cd0021c54ea957560b586b
Isayev Vladislav <vlad290897@mail.ru>
2018-02-05 15:03:00 +0300

    первый коммит

---
+++ Kino/app/src/main/java/com/example/vlad/kino/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vlad.kino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+int x = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
