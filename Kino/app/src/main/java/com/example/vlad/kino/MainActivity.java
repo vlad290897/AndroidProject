b386049ec0060ed5c8caf3bc0d1a650d28aae57e
Isayev Vladislav <vlad290897@mail.ru>
2018-02-06 12:24:51 +0300

    Добавлено навигационное меню

--- Kino/app/src/main/java/com/example/vlad/kino/MainActivity.java
+++ Kino/app/src/main/java/com/example/vlad/kino/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-int x = 1;
- String str = "первый коммит";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
