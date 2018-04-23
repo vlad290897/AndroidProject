c9936cbfe642b5f2f4032be0d8c4262965599067
Isayev Vladislav <vlad290897@mail.ru>
2018-04-23 20:01:20 +0300

    Добавил Progress Dialog

--- Afishka/app/src/main/java/com/example/vlad/a_fishka/Activity/LoginActivity.java
+++ Afishka/app/src/main/java/com/example/vlad/a_fishka/Activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.vlad.a_fishka.Activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -24,22 +25,25 @@ import com.google.firebase.auth.FirebaseUser;
  */
 
 public class LoginActivity extends Activity implements View.OnClickListener {
-    private EditText editTextEmail;
-    private EditText editTextPassword;
+    ProgressDialog pd;
     Button mSignIn;
     Button mSignUp;
-
+    private EditText editTextEmail;
+    private EditText editTextPassword;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        pd = new ProgressDialog(this);
+        pd.setTitle("Вход");
+        pd.setMessage("Авторизация");
         editTextEmail = findViewById(R.id.login);
         editTextPassword = findViewById(R.id.password);
 
 
         mAuth = FirebaseAuth.getInstance();
-
 
 
         mSignIn = findViewById(R.id.signIn);
@@ -53,9 +57,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if( user != null){
+                if (user != null) {
 
-                }else {
+                } else {
 
                 }
             }
@@ -69,46 +73,48 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.signIn:
-                if ( !RegisterActivity.isOnline(this) ) {
+                if (!RegisterActivity.isOnline(this)) {
                     Toast.makeText(getApplicationContext(),
                             "Нет соединения с интернетом!", Toast.LENGTH_LONG).show();
                     return;
 
                 }
 
-                if(email.isEmpty())
+                if (email.isEmpty())
                     editTextEmail.setError("Вы не ввели E-mail !");
-                else
-                if(password.isEmpty()&& !email.isEmpty())
+                else if (password.isEmpty() && !email.isEmpty())
                     editTextPassword.setError("Вы не ввели пароль !");
                 else
-                    signIn(email,password);
+                    signIn(email, password);
                 break;
 
 
             case R.id.signUp:
-               Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 break;
 
         }
     }
 
-    public void signIn(String email,String password){
-
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    public void signIn(String email, String password) {
+        pd.show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()) {
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                if (task.isSuccessful()) {
+                    pd.dismiss();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     Toast.makeText(LoginActivity.this, "Авторизация прошла успешно", Toast.LENGTH_LONG).show();
-                } else {Toast toast = Toast.makeText(LoginActivity.this,"Неверное имя пользователя или пароль!",Toast.LENGTH_LONG);
+                } else {
+                    pd.dismiss();
+                    Toast toast = Toast.makeText(LoginActivity.this, "Неверное имя пользователя или пароль!", Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
-                     }
+                }
             }
         });
     }
